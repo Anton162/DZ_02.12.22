df463774fd65bd94d654c050d2147daa0e3773d1
Anton <Scccq@yandex.ru>
2022-12-05 13:30:25 +0300

    дз2

---
+++ dz2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+
+if (number[0]== '-')
+{
+    if (number.Length>=4)
+    {
+        Console.WriteLine(number[3]);
+    }
+    else
+    {
+        Console.WriteLine("не трехзначное число");
+    }
+}
+else
+{
+    if (number.Length >=3)
+    {
+        Console.WriteLine(number[2]);
+    }
+    else
+    {
+        Console.WriteLine("не трехзначное число");
+    }
+}
