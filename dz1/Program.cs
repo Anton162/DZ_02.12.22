ca1b9408f7ee9b9e9af9dca77bc7d33edee7a1a5
Anton <Scccq@yandex.ru>
2022-12-05 13:15:01 +0300

    ДЗ 02.12.22.

---
+++ dz1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if((number >=100) && (number<=1000))
+{
+    int firstDigit = number % 100;
+    int secondDigit = firstDigit/10;
+    Console.WriteLine(secondDigit);
+}
+else 
+{
+    Console.WriteLine("Введено не допустимое число");
+
+}
