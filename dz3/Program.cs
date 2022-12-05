8e94d0904e4751c8957abc2c2c1fb5ac2affc0e8
Anton <Scccq@yandex.ru>
2022-12-05 14:23:17 +0300

    дз3

---
+++ dz3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите номер дня недель (1-7)");
+int n = int.Parse(Console.ReadLine()!);
+
+if (n > 0 && n < 8) {
+    if (n == 6 || n == 7) 
+    {
+        Console.WriteLine("ВЫХ");
+    } else {
+        Console.WriteLine("Буд");
+    }
+} else {
+    Console.WriteLine("Ввели неверное число");
+}
